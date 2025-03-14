4e7b78504a9635c3fb06c1be76fe40211904b636
nantasit <nantasitnsboss@gmail.com>
2025-03-14 16:05:27 +0700

    feat:HeroSection / Creade Foder img in public (add people_cat and icon google)

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './component/NavBar'
+import HeroSection from './component/HeroSection'
+
 function App() {
   return (
     <>
     <section>
       <NavBar/>
+      <HeroSection/>
     </section>  
     </>
   )

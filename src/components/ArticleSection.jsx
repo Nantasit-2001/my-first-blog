78b87b220555873ce81cbe1f94da20b4e82eab39
nantasit <nantasitnsboss@gmail.com>
2025-03-17 14:24:12 +0700

    feat: Create Component-BlogCard / render-BlogCard in ArticleSection / Create Folder-Data, File-blogPosts

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,3 +1,6 @@
+import BlogCard from "./BlogCard";
+import blogPosts from "@/Data/blogPosts";
+
 import { Input } from "./ui/input"
 import { Search } from 'lucide-react';
 import { Button } from "@/components/ui/button"
@@ -40,7 +43,7 @@ function ArticleSection (){
         <h2 className="text-3xl font-bold pb-2 px-8
                         sm:text-xl sm:px-0 lg:text-2xl
                         ">Latest articles</h2>
-        <div className='flex flex-col bg-[#EFEEEB] w-full mt-4 px-8 py-6
+          <div className='flex flex-col bg-[#EFEEEB] w-full mt-4 px-8 py-6
                         sm:flex-row items-center sm:justify-between sm:rounded-2xl sm:py-4 sm:gap-2
                         '>
             <div className="relative w-full
@@ -67,7 +70,61 @@ function ArticleSection (){
             <Button variant="LatestArticles">Inspiration</Button>
             <Button variant="LatestArticles">Ganeral</Button>
             </div>
-        </div>
+          </div>
+
+          <div className="mt-6 px-4 grid grid-cols-1 gap-12
+                          sm:mt-12 sm:px-0 sm:grid-cols-2 ">
+            <BlogCard
+              image={blogPosts[0].image}
+              category={blogPosts[0].category}
+              title={blogPosts[0].title}
+              description={blogPosts[0].description}
+              author={blogPosts[0].author}
+              date={blogPosts[0].date}
+            />
+            <BlogCard
+              image={blogPosts[1].image}
+              category={blogPosts[1].category}
+              title={blogPosts[1].title}
+              description={blogPosts[1].description}
+              author={blogPosts[1].author}
+              date={blogPosts[1].date}
+            />
+            <BlogCard
+              image={blogPosts[2].image}
+              category={blogPosts[2].category}
+              title={blogPosts[2].title}
+              description={blogPosts[2].description}
+              author={blogPosts[2].author}
+              date={blogPosts[2].date}
+            />
+            <BlogCard
+              image={blogPosts[3].image}
+              category={blogPosts[3].category}
+              title={blogPosts[3].title}
+              description={blogPosts[3].description}
+              author={blogPosts[3].author}
+              date={blogPosts[3].date}
+            />
+            <BlogCard
+              image={blogPosts[4].image}
+              category={blogPosts[4].category}
+              title={blogPosts[4].title}
+              description={blogPosts[4].description}
+              author={blogPosts[4].author}
+              date={blogPosts[4].date}
+            />
+            <BlogCard
+              image={blogPosts[5].image}
+              category={blogPosts[5].category}
+              title={blogPosts[5].title}
+              description={blogPosts[5].description}
+              author={blogPosts[5].author}
+              date={blogPosts[5].date}
+            />
+
+          </div>
+
     </section>
     </>
     )

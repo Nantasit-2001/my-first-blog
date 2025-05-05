bdf3fae45c6cc5634262235456d8e78b79caef2c
nantasit <nantasitnsboss@gmail.com>
2025-05-05 15:29:36 +0700

    feat: add profile image upload form and send image to server

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+import { commonjs } from "globals";
 
 const API_URL = `${import.meta.env.VITE_API_URL}/profile`;
+
+export const axiosFetchUser = ()=>{
+  return axios.patch(API_URL)
+}
 
 export const axiosResetPassword = ({currentPassword, newPassword}) =>{
     return axios.patch(API_URL+"/reset-password",{currentPassword, newPassword},
@@ -12,13 +17,16 @@ export const axiosResetPassword = ({currentPassword, newPassword}) =>{
       );
 } 
 
-export const axiosResetProfile = ({image, name, username}) =>{
-  console.log(image,name,username)
-  return axios.patch(API_URL+"/reset-profile", {image,name,username},
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      );
-}
+export const axiosResetProfile = ({ image, name, username }) => {
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("username", username);
+  formData.append("image", image);
+  console.log("---------------------")
+  return axios.patch(API_URL + "/reset-profile", formData, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
